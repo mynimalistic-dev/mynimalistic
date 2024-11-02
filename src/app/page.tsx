e610da8fd65000d400e2a8daf16e7a933cec3b09
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-02 16:39:58 +0530

    feat: Add FeaturedSection and ServicesSection to Home component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { HomeContent } from "@/components/home-content";
+import { FeaturedSection } from "@/components/FeaturedSection";
+import { ServicesSection } from "@/components/ServicesSection";
 
 export default function Home() {
-  return <HomeContent />;
+  return (
+    <main className="relative min-h-screen bg-gradient-to-b from-sky-50 via-white to-sky-50 dark:from-black dark:via-black dark:to-black">
+      <HomeContent />
+      <FeaturedSection />
+      <ServicesSection />
+    </main>
+  );
 } 
