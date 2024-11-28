fa88b4c25dfc5ccc0b393deea8cd5ea370e0be6b
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-28 18:47:12 +0530

    feat: Update ThemeProvider import to use next-themes for improved theming support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes"
 
 const inter = Inter({ subsets: ["latin"] });
 
