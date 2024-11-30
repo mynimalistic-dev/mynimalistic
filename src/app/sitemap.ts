20a6fb871cc2ce4f212bdb7b1b8582db9a4cd3c6
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-30 12:59:48 +0530

    feat: Update sitemap with new URLs and change canonical domain to myminimalistic.tech

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,23 +3,46 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mynimalistic.vercel.app',
+      url: 'https://www.mynimalistic.tech',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://mynimalistic.vercel.app/about',
+      url: 'https://www.mynimalistic.tech/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://mynimalistic.vercel.app/services',
+      url: 'https://www.mynimalistic.tech/services',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
-    // Add more URLs as needed
+    {
+      url: 'https://www.mynimalistic.tech/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url:"https://www.mynimalistic.tech/projects",    
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.mynimalistic.tech/privacy-policy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+     url: 'https://www.mynimalistic.tech/terms',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    }
   ]
 } 
